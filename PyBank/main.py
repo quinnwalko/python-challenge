aa02d005309490374cd7e227919049c7d02046d7
quinnwalko <quinn.walko@gmail.com>
2018-10-27 18:26:57 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,13 @@
 import os
 import csv
 
-budget.csv = os.path.join('..', 'Resources', 'budget_data.csv')
+budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
 
 date[]
 profit_losses[]
+
+with open(budget_csv, newline="") as csvfile:
+  csvreader = csv.reader(csvfile, delimiter=",")
+  for row in csvreader:
+    date.append(row[1])
+    
