b15c679fcda50c5bdab951191fd654a8770312b7
quinnwalko <quinn.walko@gmail.com>
2018-10-28 18:45:18 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,5 +15,11 @@ for row in csvreader:
     Number_of_Rows = len(list(csvreader))
     print(Number_of_Rows)
     
+with open("budget_data.csv") as csv_file:
+    csvreader = csv.DictReader(csv_file)
+    total = sum(float(row["Profit/Losses"]) for row in csvreader)
+print(total)
+    
+    
     
     
