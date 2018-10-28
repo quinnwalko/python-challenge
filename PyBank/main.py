e02c78ace883f91f20ebf7e7b1ef31108a31fe5a
quinnwalko <quinn.walko@gmail.com>
2018-10-28 19:23:03 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,11 +7,8 @@ csvRows = []
 
 csvFileObj = open("budget_data.csv")
 csvreader = csv.reader(csvFileObj)
+
 for row in csvreader:
-    if csvreader.line_num == 1:
-        continue
-    csvRows.append(row)
-    
     Number_of_Rows = len(list(csvreader))
     print(Number_of_Rows)
     
