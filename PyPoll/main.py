fe5d57ab13e282e9368092b6cf5af5d09be2296b
quinnwalko <quinn.walko@gmail.com>
2018-10-28 20:41:01 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,14 @@
+import os
+import csv
+
+csvpath = os.path.join("..", "C:/Users/quinn/AppData/Local/Programs/Python/Python37-32", "election_data.csv")
+
+csvRows = []
+
+csvFileObj = open("election_data.csv")
+csvreader = csv.reader(csvFileObj)
+
+for row in csvreader:
+    Number_of_Votes = len(list(csvreader))
+    print(Number_of_Votes)
 
