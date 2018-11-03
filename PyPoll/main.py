6e2295bb93c29c5b6c5200d12c40d1073e4f78c1
quinnwalko <quinn.walko@gmail.com>
2018-11-02 20:50:50 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,14 @@ election_data = "election_data.csv"
 election_data_df = pd.read_csv(election_data)
 election_data_df.head()
 
+#Number of total voters
 Total_Votes = election_data_df["Voter ID"].value_counts().sum
 Total_Votes
+
+#Candidates receiving votes/number of votes for each candidate
+vote_count_df = election_data_df["Candidate"].value_counts()
+vote_count_df
+
+#Percentage of the vote for each candidate
+election_data_df.groupby(["Candidate"]).sum()/election_data_df["Voter ID"].sum() * 100
 
